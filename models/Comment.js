b130f6bad4286f3dabcfb9d22b3a30df20e1a860
Ben Marley <benjaminjohnmarley@gmail.com>
2018-06-28 12:04:56 +0100

    refactor comment model fixes timestamp issue

--- models/Comment.js
+++ models/Comment.js
@@ -1,27 +1,39 @@
 const mongoose = require('mongoose');
+
 const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
   body: {
     type: String,
+
     required: true
   },
+
   belongs_to: {
     type: mongoose.Schema.Types.ObjectId,
+
     ref: 'articles',
+
     required: true
   },
+
   created_at: {
     type: Number,
-    default: new Date().getTime()
+
+    default: () => new Date().getTime()
   },
+
   votes: {
     type: Number,
+
     default: 0
   },
+
   created_by: {
     type: mongoose.Schema.Types.ObjectId,
+
     ref: 'users',
+
     required: true
   }
 });
